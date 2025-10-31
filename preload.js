3cfd7a37fdbd63abf8eaed6c706a5c5eef67c011
hypn05 <hypn05>
2025-10-30 22:41:00 -0500

    feat: implement Electron main process with sidebar window and tray icon

---
+++ preload.js
@@ -0,0 +1,23 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  loadNotes: () => ipcRenderer.invoke('load-notes'),
+  saveNotes: (notes) => ipcRenderer.invoke('save-notes', notes),
+  minimizeWindow: () => ipcRenderer.invoke('minimize-window'),
+  closeWindow: () => ipcRenderer.invoke('close-window'),
+  openExternal: (url) => ipcRenderer.invoke('open-external', url),
+  scheduleReminder: (reminder) => ipcRenderer.invoke('schedule-reminder', reminder),
+  showNotification: (options) => ipcRenderer.invoke('show-notification', options),
+  onNotificationClick: (callback) => ipcRenderer.on('notification-clicked', (event, noteId) => callback(noteId)),
+  onCreateNewNote: (callback) => ipcRenderer.on('create-new-note', () => callback()),
+  onNotesImported: (callback) => ipcRenderer.on('notes-imported', () => callback()),
+  resizeWindow: (width, height) => ipcRenderer.invoke('resize-window', width, height),
+  hideWindow: () => ipcRenderer.invoke('hide-window'),
+  showWindow: () => ipcRenderer.invoke('show-window'),
+  getWindowSize: () => ipcRenderer.invoke('get-window-size'),
+  setIgnoreMouseEvents: (ignore) => ipcRenderer.invoke('set-ignore-mouse-events', ignore),
+  getTheme: () => ipcRenderer.invoke('get-theme'),
+  onThemeChanged: (callback) => ipcRenderer.on('theme-changed', (event, theme) => callback(theme)),
+  getStayInView: () => ipcRenderer.invoke('get-stay-in-view'),
+  onStayInViewChanged: (callback) => ipcRenderer.on('stay-in-view-changed', (event, stayInView) => callback(stayInView))
+});
